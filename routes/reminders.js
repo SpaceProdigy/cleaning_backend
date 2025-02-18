1d744436ef8c824ac93e1835175af9e63054186d
OlegatorIT <podlesnyoleg93@gmail.com>
2025-02-18 01:01:32 +0100

    auto fast commit

--- routes/reminders.js
+++ routes/reminders.js
@@ -1,11 +1,11 @@
 import express from "express";
 
-import { schedulesReminder } from "../telegram/reminders/autoCleaningReminder.js";
+// import { schedulesReminder } from "../telegram/reminders/autoCleaningReminder.js";
 import { cleaningReminder } from "../telegram/reminders/cleaningReminder.js";
 
 const reminders = express.Router();
 
-reminders.get("/autoReminder", schedulesReminder);
+// reminders.get("/autoReminder", schedulesReminder);
 
 reminders.post("/:group", cleaningReminder);
 
